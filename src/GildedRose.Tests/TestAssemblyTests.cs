c7a5804ca47d4ea5153aa3830e95ce8e307006a7
Aitor Alzola <alzola.aitor@gmail.com>
2017-09-17 10:04:22 +0200

    Step 1 - Prepared with tests

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,77 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
+using GildedRose.Console;
 
 namespace GildedRose.Tests
 {
+    using FluentAssertions;
+
     public class TestAssemblyTests
     {
         [Fact]
-        public void TestTheTruth()
+        public void TestTheValues_WhenOneDayPass()
         {
-            Assert.True(true);
+            var inventory = new ItemsInventory()
+            {
+                Items = new List<Item>
+                        {
+                            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                            new Item
+                                {
+                                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                                    SellIn = 15,
+                                    Quality = 20
+                                },
+                            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                        }
+
+            };
+
+            inventory.UpdateQuality();
+
+            inventory.Items[0].Quality.Should().Be(19);
+            inventory.Items[1].Quality.Should().Be(1);
+            inventory.Items[2].Quality.Should().Be(6);
+            inventory.Items[3].Quality.Should().Be(80);
+            inventory.Items[4].Quality.Should().Be(21);
+            inventory.Items[5].Quality.Should().Be(5);
+        }
+
+        [Fact]
+        public void TestTheValues_WhenTwoDaysPass()
+        {
+            var inventory = new ItemsInventory()
+            {
+                Items = new List<Item>
+                        {
+                            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                            new Item
+                                {
+                                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                                    SellIn = 15,
+                                    Quality = 20
+                                },
+                            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                        }
+
+            };
+
+            inventory.UpdateQuality();
+            inventory.UpdateQuality();
+
+            inventory.Items[0].Quality.Should().Be(18);
+            inventory.Items[1].Quality.Should().Be(2);
+            inventory.Items[2].Quality.Should().Be(5);
+            inventory.Items[3].Quality.Should().Be(80);
+            inventory.Items[4].Quality.Should().Be(22);
+            inventory.Items[5].Quality.Should().Be(4);
         }
     }
 }
