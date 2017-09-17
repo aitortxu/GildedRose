dc1a4e99eb09a8467880e839d2cb1e921b1f19e4
Aitor Alzola <alzola.aitor@gmail.com>
2017-09-17 10:04:22 +0200

    More tests

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRose.Console
 {
     class Program
-    {
-        IList<Item> Items;
+    {        
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
+            var inventory = new ItemsInventory()
                           {
                               Items = new List<Item>
                                           {
@@ -28,97 +28,21 @@ namespace GildedRose.Console
 
                           };
 
-            app.UpdateQuality();
+            for (int i = 1; i < 20; i++)
+            {                
+                System.Console.WriteLine(string.Format("Iteration {0} -----------------------", i) );
+                inventory.UpdateQuality();
+
+                foreach (var item in inventory.Items)
+                {
+                    System.Console.WriteLine(string.Format("Updated quality of {0} is {1}", item.Name, item.Quality));
+                    System.Console.WriteLine(string.Format("Updated sellin of {0} is {1}", item.Name, item.SellIn));
+                }
+            }
+
 
             System.Console.ReadKey();
 
         }
-
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,91 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
+using GildedRose.Console;
 
 namespace GildedRose.Tests
 {
+    using FluentAssertions;
+
     public class TestAssemblyTests
     {
         [Fact]
-        public void TestTheTruth()
+        public void TestTheValues_WhenOneDayPass()
         {
-            Assert.True(true);
+            var inventory = new ItemsInventory()
+            {
+                Items = new List<Item>
+                        {
+                            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                            new Item
+                                {
+                                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                                    SellIn = 15,
+                                    Quality = 20
+                                },
+                            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                        }
+
+            };
+
+            inventory.UpdateQuality();
+
+            inventory.Items[0].Quality.Should().Be(19);
+            inventory.Items[1].Quality.Should().Be(1);
+            inventory.Items[2].Quality.Should().Be(6);
+            inventory.Items[3].Quality.Should().Be(80);
+            inventory.Items[4].Quality.Should().Be(21);
+            inventory.Items[5].Quality.Should().Be(5);
+        }
+
+        [Fact]
+        public void TestTheValues_WhenTwentyDaysPass()
+        {
+            var inventory = new ItemsInventory()
+            {
+                Items = new List<Item>
+                        {
+                            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                            new Item
+                                {
+                                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                                    SellIn = 15,
+                                    Quality = 20
+                                },
+                            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                        }
+
+            };
+
+            for (int i = 1; i < 20; i++)
+            {
+                inventory.UpdateQuality();
+
+            }
+
+            inventory.Items[0].Quality.Should().Be(0);
+            inventory.Items[0].SellIn.Should().Be(-9);
+
+            inventory.Items[1].Quality.Should().Be(36);
+            inventory.Items[1].SellIn.Should().Be(-17);
+
+            inventory.Items[2].Quality.Should().Be(0);
+            inventory.Items[2].SellIn.Should().Be(-14);
+
+            inventory.Items[3].Quality.Should().Be(80);
+            inventory.Items[3].SellIn.Should().Be(0);
+
+            inventory.Items[4].Quality.Should().Be(0);
+            inventory.Items[4].SellIn.Should().Be(-4);
+
+            inventory.Items[5].Quality.Should().Be(0);
+            inventory.Items[5].SellIn.Should().Be(-16);
         }
     }
 }
